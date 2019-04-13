633073a6e541452c62b5406380ce886f5eef4cc3
Nimander <szymanskimichal57@gmail.com>
2019-04-13 10:46:01 +0200

    add test data

---
+++ data.py
@@ -0,0 +1,28 @@
+tag = [{"_id": 123, "name": "info", "tag_size": 2},
+	   {"_id": 124, "name": "fun", "tag_size": 1},
+	   {"_id": 125, "name": "sport", "tag_size": 1}]
+
+# zmienic createdDate na timestamp
+# dodac title
+storage = [
+	{"_id": 531,
+	 "description": "desc1",
+	 "source": "www.interia.pl",
+	 "tags": ["info"],
+	 "link": "https://www.interia.pl",
+	 "createdDate": "01-01-2018"},
+
+	{"_id": 532,
+	 "description": "desc2",
+	 "source": "www.sport.pl",
+	 "tags": ["info", "sport"],
+	 "link": "https://www.sport.pl",
+	 "createdDate": "03-10-2018"},
+
+	{"_id": 533,
+	 "description": "desc3",
+	 "source": "www.facebook.com",
+	 "tags": ["fun"],
+	 "link": "https://www.facebook.com",
+	 "createdDate": "12-05-2018"},
+]
