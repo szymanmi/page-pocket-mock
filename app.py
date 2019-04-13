19cf14da4a0323bb683921e628fbaefe5be0f560
Nimander <szymanskimichal57@gmail.com>
2019-04-13 10:49:38 +0200

    add getting by tag

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
+import data
 
 app = Flask(__name__)
 
@@ -6,6 +8,19 @@ app = Flask(__name__)
 @app.route("/api/test")
 def hello():
 	return "Hello World!"
+
+
+# example: http://127.0.0.1:8013/api/getbytag?tag=info
+@app.route("/api/getbytag")
+def getbytag():
+	new_list = []
+	requested_tag = request.args.get('tag')
+	for item in data.storage:
+		if requested_tag in item["tags"]:
+			new_list.append(item)
+
+	new_list = json.dumps(new_list)
+	return new_list
 
 
 if __name__ == '__main__':
